3ea881a0e2be869219c9740c54891faa01b04c65
ioana gheorghias <ioana.gh27@gmail.com>
2022-01-13 13:38:12 +0000

    no change

--- reddit_project/src/containers/redditList/RedditList.js
+++ reddit_project/src/containers/redditList/RedditList.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
-import { changeRedditId } from "../individualReddit/individualRedditSlice";
 import { RedditComponent } from '../redditComponent/RedditComponent';
 import {  setReddits } from "../redditComponent/redditSlice";
 import { SubredditMenu } from "../subredditMenu/SubredditMenu";
